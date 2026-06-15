0b7ba53620abab656a1ae25d339175f83838f48b
davisyegon-lab <davis.yegon@strathmore.edu>
2026-06-15 23:45:19 +0300

    First commit - 15 June 2026

---
+++ script.js
@@ -0,0 +1,36 @@
+function greetings(fname) {
+    var dayTime = new Date();
+    var hours = dayTime.getHours();
+    var greeting;
+    if (hours < 12) {
+        greeting = "Good morning!";
+    } else if (hours < 18) {
+        greeting = "Good afternoon!";
+    } else {
+        greeting = "Good evening!";
+    }
+    return greeting + " " + fname + "!";
+}
+
+function starTime() {
+    //get the current date and time
+    var today = new Date();
+
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var dayOfWeek = days[today.getDay()]; //Get the current day of the week
+
+    var day = today.getDate();
+
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var month = month[today.getMonth()]; //Months are zero-based
+
+    var year = today.getFullYear();
+
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+    document.getElementById("realTime").innerHTML = dayOfWeek+ ", " + day + " " + month + " " +
+    year + " " + formatTime(hours)+ ":" + formatTime(minutes) + ":" + formatTime(second);
+    t=setTimeout(starTime,1000)
+
+}
